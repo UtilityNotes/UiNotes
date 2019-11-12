4ac18e139af4ad7318522958e8e1b9bb1cf83f02
Mister Mjir <39204944+MisterMjir@users.noreply.github.com>
2019-11-11 21:41:47 -0600

    Documentation (#12)
    * Update LOG.md
    
    Changed formatting
    
    * Update LOG.md
    
    * Update LOG.md
    
    Fixed formatting
    
    * Update LOG.md
    
    * Update documentation, fix stuff

--- ui/menu.py
+++
@@ -1,7 +0,0 @@
-import UiNotes
-
-def main():
-    title = UiNotes.runJS("document.title")
-    print("The current file is " + title)
-    UiNotes.runJS("$('body').append(\"<div id='title' style='width:calc(100% - 8px);height:64px;box-shadow:0 1px 3px rgba(0,0,0,.8);border-radius:4px;padding:4px'></div>\")")
-    UiNotes.runJS("$('#title').append(\"<h1 style='text-align:center;color:rgba(255,255,255,.8)''>The Menu</h1>\")")
